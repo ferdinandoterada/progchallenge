fa77a2bcbabecb7b7b9522a0b1abfb9594c81b21
ferdinandoterada <ferdinandu@gmail.com>
2023-01-12 21:33:59 -0300

    Include all done exercises
    Signed-off-by: ferdinandoterada <ferdinandu@gmail.com>

---
+++ exercise_10/CalculateLength.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    std::string s = "";
+    std::cout << "Type string to calculate length: ";
+    std::cin >> s;
+    
+    int i = 0;
+    while (s[i])
+        i++;
+    
+    std::cout << "Length of string '" << s << "' is " << i << std::endl;
+    
+    
+    return 0;
+}
---
+++ exercise_3/CountTo100.cpp
@@ -0,0 +1,9 @@
+int main()
+{
+    for( auto count = 1; count <= 100; ++count){
+        if( count % 3 == 0 ) cout << "Foo" << endl;
+        else if ( count % 5 == 0 ) cout << "Baa" << endl;
+        else if ( count % 5 == 0 and count % 3 == 0) cout << "FooBaa" << endl;
+        else cout << count << endl;
+    }
+}
